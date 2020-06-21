6fbe8631af5f0d34dcca97323e4a0251cc86065b
himasheth <himasheth05@gmail.com>
2020-06-21 15:27:47 -0400

    update game and js

--- website.js
+++ website.js
@@ -1,28 +1,32 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var index = 1;
+showSlides(index);
 
 // Next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlides(index += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlides(index = n);
 }
 
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) {
+        index = 1
+    }
+    if (n < 1) {
+        index = slides.length
+    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    slides[index - 1].style.display = "block";
+    dots[index - 1].className += " active";
 }
