c845748efc6d0685b5a48bf63850ef211b4b8163
himasheth <himasheth05@gmail.com>
2020-06-21 15:23:31 -0400

    add logo and fix slideshow

--- website.js
+++ website.js
@@ -1,32 +1,28 @@
-var index = 1;
-showSlides(index);
+var slideIndex = 1;
+showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-    showSlides(sliindexdeIndex += n);
+    showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-    showSlides(index = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        index = 1
-    }
-    if (n < 1) {
-        index = slides.length
-    }
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[index - 1].style.display = "block";
-    dots[index - 1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
