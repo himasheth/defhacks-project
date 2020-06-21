1414eabddf4c8a685f42822bf1e5630a2131dc43
himasheth <himasheth05@gmail.com>
2020-06-21 13:52:42 -0400

    addition of website.js

---
+++ website.js
@@ -0,0 +1,32 @@
+var index = 1;
+showSlides(index);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(sliindexdeIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(index = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        index = 1
+    }
+    if (n < 1) {
+        index = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[index - 1].style.display = "block";
+    dots[index - 1].className += " active";
+}
